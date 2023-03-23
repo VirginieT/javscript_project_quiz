27b96cc53e3af001e860def82c0915e02241024c
VirginieT <vtouzalin33@gmail.com>
2023-03-23 14:54:57 +0100

    modify controler.js

---
+++ js/controler.js
@@ -0,0 +1,6 @@
+const start = document.querySelector('.start');
+const title = document.querySelector('.title');
+const start_image = document.querySelector('.start_image');
+const info = document.querySelector('.info');
+const start_btn = document.querySelector('.start_btn');
+
