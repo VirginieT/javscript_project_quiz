16ca11b6a8c05df320aaedb04f98fe2b02685d6f
VirginieT <vtouzalin33@gmail.com>
2023-03-23 15:01:11 +0100

    JS const creation

--- js/controler.js
+++ js/controler.js
@@ -3,4 +3,20 @@ const title = document.querySelector('.title');
 const start_image = document.querySelector('.start_image');
 const info = document.querySelector('.info');
 const start_btn = document.querySelector('.start_btn');
+const quiz = document.querySelector('.quiz');
+const progress = document.querySelector('.progress');
+const score = document.querySelector('.score');
+const question = document.querySelector('.question');
+const option = document.querySelector('.option');
+const result = document.querySelector('.result');
+const total_question = document.querySelector('.total_question');
+const next_btn = document.querySelector('.next_btn');
+const next = getElementById('next');
+const end = document.querySelector('.end');
+const end_image = document.querySelector('.end_image');
+const end_score = document.querySelector('.end_score');
+const end_btn = document.querySelector('.end_btn');
+const restart = getElementById('restart');
+const quit = getElementById('quit');
+
 
