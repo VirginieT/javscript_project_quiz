f8d8e9dff5df354dd7cc4e2fe83e9fb006388504
VirginieT <vtouzalin33@gmail.com>
2023-03-23 15:37:42 +0100

    update start section

--- js/controler.js
+++ js/controler.js
@@ -1,6 +1,7 @@
 const start = document.querySelector('.start');
 const title = document.querySelector('.title');
 const start_image = document.querySelector('.start_image');
+const welcome = document.querySelector('.welcome');
 const info = document.querySelector('.info');
 const start_btn = document.querySelector('.start_btn');
 const quiz = document.querySelector('.quiz');
