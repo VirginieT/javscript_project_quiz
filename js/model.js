7eb3f91999db6efe6278a10c4c27afb20ad1f9fa
pitrouA <adrien.pitrou@gmail.com>
2023-03-23 12:02:46 +0100

    adding question, reponse and questionnaire in model.js

--- js/model.js
+++ js/model.js
@@ -1,18 +1,81 @@
-function Question(s_intitule, b_tableQuestions, i_nbQuestions){
-  var s_intitule = "";
-  var b_tableQuestions = [];
-  var i_nbQuestions = 4;
+class Reponse {
+  constructor(s_intitule, b_valide) {
+    this.s_intitule = s_intitule;
+    this.b_valide = b_valide;
+  }
+
+  contenuHTML(){
+    return "<p>Intitule de la reponse</p>"
+  }
 }
 
-Question.prototype.contenuHTML = function() {
-  return "<p>Intitule de la question</p>"
-};
+class Question {
+  constructor(s_intitule, tab_tableReponse) {
+    this.s_intitule = s_intitule;
+    this.tab_tableReponse = tab_tableReponse;
+  }
 
+  addReponse(reponse){
+    this.tab_tableReponse.append(reponse);
+  }
 
-function QuestionCheck(s_intitule, b_tableQuestions, i_nbQuestions){
-  Question.call(this, s_intitule, b_tableQuestions, i_nbQuestions);
+  contenuHTML(){
+    var ret = "";
+    for(var i=0;i<this.tab_tableReponse.length;i++){
+      ret = ret + this.tab_tableReponse[i].contenuHTML() + "<br>"
+    }
+    return "<p>Intitule de la question</p>"+ret;
+  }
 }
-QuestionCheck.prototype = Object.create(Question.prototype);
 
-var questCheck = new QuestionCheck("fdgsdgf",[true, false, true, true], 4);
-console.log("quest ch"+questCheck);
+class QuestionCheck extends Question {
+  constructor(s_intitule, tab_tableReponse) {
+    super(s_intitule, tab_tableReponse);
+  }
+}
+
+class QuestionRadio extends Question {
+  constructor(s_intitule, tab_tableReponse) {
+    super(s_intitule, tab_tableReponse);
+  }
+
+  contenuHTML(){
+    var ret = "";
+    for(var i=0;i<this.tab_tableReponse.length;i++){
+      ret = ret +"<input type='radio'>" + this.tab_tableReponse[i].contenuHTML() + "</input><br>"
+    }
+    return "<p>Intitule de la question</p>"+ret;
+  }
+}
+
+class Questionnaire {
+  constructor(tab_questions) {
+    this.tab_questions = tab_questions;
+  }
+
+  addQuestion(question){
+    this.tab_questions.append(question);
+  }
+
+  contenuHTML(){
+    var ret = "";
+    for(var i=0;i<this.tab_questions.length;i++){
+      ret = ret + this.tab_questions[i].contenuHTML() + "<br>"
+    }
+    return "<p>Questionnaire : </p>"+ret;
+  }
+}
+
+
+//to test
+
+var tabRep = [new Reponse("1",true), new Reponse("2",false)];
+var questionCheck = new QuestionCheck("Quel est le nom de mon petit frère", tabRep);
+
+var tabRep = [new Reponse("1",true), new Reponse("2",false)];
+var questionRadio = new QuestionRadio("La Radio est-elle utile ?", tabRep);
+
+var questionnaire = new Questionnaire([questionCheck, questionRadio]);
+
+console.log("questionnaire = "+JSON.stringify(questionnaire))
+console.log("contenu = "+questionnaire.contenuHTML())
