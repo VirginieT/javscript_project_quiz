263a4b36ca9641c586cf544d1096e4636525bf15
VirginieT <vtouzalin33@gmail.com>
2023-03-24 15:45:19 +0100

    add question to progress

--- js/controller.js
+++ js/controller.js
@@ -125,7 +125,7 @@ document.addEventListener("DOMContentLoaded", () => {
 function showNext(){
   //remove all valid and invalid class to hide result
   question.innerHTML = questionnaire.contenuHTML().replaceAll('invalid', '').replaceAll('valid', '')
-  document.querySelector('.progress').innerHTML = questionnaire.indexQuestion+"/"+questionnaire.nbQuestion;
+  document.querySelector('.progress').innerHTML = questionnaire.indexQuestion+" / "+questionnaire.nbQuestion+" questions";
 
   //enable dir evt
   var divs = document.getElementsByClassName("options")
